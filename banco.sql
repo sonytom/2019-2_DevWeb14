aab7125b29fbcb356ab5c6c2c6117a7801bf54e9
tom <fichtner.dk@hotmail.com>
2019-09-26 20:49:13 -0300

    addbanco

---
+++ banco.sql
@@ -0,0 +1,22 @@
+CREATE TABLE usuarios (
+id          		BIGINT (20) NOT NULL AUTO_INCREMENT,
+dataHora   			TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+nome        		TEXT NOT NULL,
+login       		VARCHAR (50) NOT NULL,
+senha      			VARCHAR (50) NOT NULL,
+dataNascimento		DATE NOT NULL,
+altura				float(10,2) NOT NULL,
+CONSTRAINT pk_usuarios PRIMARY KEY(id)) ENGINE = InnoDB COLLATE = latin1_general_ci;
+
+INSERT INTO usuarios (nome ,login, senha, dataNascimento, altura) VALUES ( 'Usuário Aluno Teste','aluno', MD5('123456'),'1982-10-06',1.7);
+
+CREATE TABLE cadastros (
+id                  BIGINT (20) NOT NULL AUTO_INCREMENT,
+data_hora           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+nome	            VARCHAR (100) NOT NULL,
+sexo                CHAR (2) NOT NULL,
+descricao			TEXT NOT NULL,
+idCadastroUsuario BIGINT (20) NOT NULL,
+CONSTRAINT pk_cadastros PRIMARY KEY(id),
+CONSTRAINT fk_cadastros_usuarios FOREIGN KEY(id_cadastro_usuario) REFERENCES usuarios(id)) 
+
